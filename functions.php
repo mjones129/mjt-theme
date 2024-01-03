de64fa4daaf63af86b56b33eaa531a2e40c44ac1
Matt Jones <29353833+mjones129@users.noreply.github.com>
2024-01-02 21:39:02 -0500

    its getting late and stuff is broken

--- functions.php
+++ functions.php
@@ -4,11 +4,11 @@ function add_style() {
   //custom styles
   wp_enqueue_style('blocks-styles', get_stylesheet_uri());
   //particles
-  wp_enqueue_script('pjs', get_template_directory_uri() . '/node_modules/particlesjs/dist/particles.min.js', [], '2.0.0', []);
   wp_enqueue_script('particle-instance', get_template_directory_uri() . '/js/particles.js', [], '1.0.0', []);
+  wp_enqueue_script('pjs', get_template_directory_uri() . '/node_modules/particlesjs/dist/particles.min.js', [], '2.0.0', []);
   //anime js
-  wp_enqueue_script('anime-core', get_template_directory_uri() . '/node_modules/animejs/lib/anime.min.js', [], null, []);
-  wp_enqueue_script('anime-config', get_template_directory_uri(). '/js/cards.js', [], null, []);
+  // wp_enqueue_script('anime-core', get_template_directory_uri() . '/node_modules/animejs/lib/anime.min.js', [], null, []);
+  // wp_enqueue_script('anime-config', get_template_directory_uri(). '/js/cards.js', [], null, []);
   //bootstrap
   wp_enqueue_style('bootstrap-css', get_template_directory_uri() . '/node_modules/bootstrap/dist/css/bootstrap.min.css');
   wp_enqueue_script('bootstrap-js', get_template_directory_uri() . '/node_modules/bootstrap/dist/js/bootstrap.min.js', [], null, []);
--- js/particles.js
+++ js/particles.js
@@ -1,4 +1,5 @@
-import { particlesJS } from "particlesjs";
-particlesJS.load("particles-js", "/wp-content/themes/themegen/assets/particles.json", function () {
-  console.log("callback - particles.js config loaded");
+import '../node_modules/particlesjs';
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', '../assets/particles.json', function() {
+  console.log('callback - particles.js config loaded');
 });
