5c9afd48196343fe4bfb7967d44f9a981efb7fa6
Matt Jones <29353833+mjones129@users.noreply.github.com>
2024-01-04 17:27:40 -0500

    add update function

--- functions.php
+++ functions.php
@@ -1,4 +1,36 @@
 <?php
+//Check GitHub for updates
+function automatic_Github_updates($data) {
+  //Theme Information
+  $theme = get_stylesheet(); //folder name of the current theme
+  $current = wp_get_theme()->get('Version'); //Get the version of the current theme
+
+  //Github Information
+  $user = 'mjones129'; //Github username hosting the repository
+  $repo = 'mjt-theme'; //Repository name as it appears in the URL
+
+  //Get the latest release tag from the repository. The User-Agent header must be sent, as per Github's API documentation: https://developer.github.com/v3/#user-agent-required
+  $file = @json_decode(@file_get_contents('https://api.github.com/repos/'.$user.'/'.$repo.'/releases/latest', false, stream_context_create(['http' => ['header' => "User-Agent: ".$user."/r/n"]])
+  ));
+  if($file) {
+    $update = filter_var($file->tag_name, FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+    //only return a response if there is a new version number higher than the current version
+    if($update > $current) {
+      $data->response[$theme] = array(
+        'theme' => $theme,
+        //Strip the version number of any non-alpha characters (excluding the period)
+        //That way you can still use tags like v1.1 or ver1.1 if desired
+        'new_version' => $update,
+        'url' => 'https://github.com/'.$user.'/'.$repo,
+        'package' => $file->assets[0]->browser_download_url,
+      );
+    }
+  }
+  return $data;
+}
+add_filter('pre_set_site_transient_update_themes', 'automatic_Github_updates');
+
+
 //Add stylesheet
 function add_style() {
   //particles
