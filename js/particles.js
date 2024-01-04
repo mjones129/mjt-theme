6f37f8580a9ca050f05f4132bbc3e42d8483a692
Matt Jones <29353833+mjones129@users.noreply.github.com>
2024-01-04 04:41:26 -0500

    add ts particles engine, begin tsparticles config

--- js/particles.js
+++
@@ -1,5 +0,0 @@
-import '../node_modules/particlesjs';
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', '../assets/particles.json', function() {
-  console.log('callback - particles.js config loaded');
-});
