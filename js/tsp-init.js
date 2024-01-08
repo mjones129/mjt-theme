c11f8fe3f83ca68708e848e2725e2775ff8b1f5f
Matt Jones <me@mattjones.tech>
2024-01-07 20:11:53 -0500

    remove unused particles config

--- js/tsp-init.js
+++
@@ -1,7 +0,0 @@
-(async () => {
-  await loadLinksPreset(tsParticles); // this is required only if you are not using the bundle script
-
-  await tsParticles.load("tsparticles", {
-    preset: "links",
-  });
-})();
