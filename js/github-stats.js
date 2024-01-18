e2de071c85bdd1fa2d82104227ea58187ecec31a
Matt Jones <me@mattjones.tech>
2024-01-18 05:13:35 -0500

    add github graphql query

---
+++ js/github-stats.js
@@ -0,0 +1,47 @@
+// const { githubToken } = require('../includes/github-api-token.js');
+import { githubToken } from '../includes/github-api-token.js';
+
+const endpoint = "https://api.github.com/graphql";
+const headers = {
+	"content-type": "application/json",
+    "Authorization": githubToken
+};
+const graphqlQuery = {
+    "query": `query { user(login: "mjones129") {
+      contributionsCollection {
+        contributionCalendar {
+          weeks {
+            contributionDays {
+              contributionCount
+              weekday
+              date
+            }
+          }
+        }
+      }
+    }
+  }`,
+    "variables": {}
+};
+
+const options = {
+    "method": "POST",
+    "headers": headers,
+    "body": JSON.stringify(graphqlQuery)
+};
+
+async function listen() {
+  const response = await fetch(endpoint, options);
+  const data = await response.json();
+
+  console.log(data.data);
+  console.log(data.errors);
+
+  let weeks = JSON.stringify(data.data.user.contributionsCollection.contributionCalendar.weeks);
+
+  document.getElementById('code').innerHTML = js_beautify(weeks); 
+}
+
+listen();
+
+
