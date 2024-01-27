66d11227606843ef80f9c7753535c39ef4b0595d
Matt Jones <me@mattjones.tech>
2024-01-27 10:49:05 -0500

    remove unused scripts from import

--- js/main.js
+++ js/main.js
@@ -1,8 +1,5 @@
 import { cardsUp } from './cards.js';
-// import { githubToken } from '../includes/github-api-token.js';
 import { Chart } from 'chart.js/auto';
 import { listen } from './github-stats.js';
-// import { anime } from 'animejs';
-// import { js_beautify } from 'js-beautify';
 
 cardsUp();
